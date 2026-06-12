603328ec0c87b3d5ee11ef1c083dfd05a9096f32
Sk <shivamkmr2211@gmail.com>
2026-06-12 12:23:32 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -35,23 +35,9 @@ function App() {
         }}
       >
         {filteredCountries.map((country) => (
-          <div
-            key={country.common}
-            className="countryCard"
-            style={{
-              border: "1px solid #ccc",
-              width: "150px",
-              padding: "10px",
-              textAlign: "center",
-            }}
-          >
-            <img
-              src={country.png}
-              alt={country.common}
-              width="100"
-              height="60"
-            />
-            <h3>{country.common}</h3>
+          <div key={country.common} className="countryCard">
+            <img src={country.png} alt={`Flag of ${country.common}`} />
+            <p>{country.common}</p>
           </div>
         ))}
       </div>
